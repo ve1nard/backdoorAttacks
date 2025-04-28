018708dbfaff3d4ece5b8e9f2e9ce4160bbd3200
Vladislav Zapromyotov <93820988+ve1nard@users.noreply.github.com>
2025-04-29 00:09:53 +0400

    Delete baddet.py

--- baddet.py
+++
@@ -1,8 +0,0 @@
-import sys
-import argparse
-from setup import set_args
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=sys.argv[0])
-    set_args(parser)
-    args = parser.parse_args()
